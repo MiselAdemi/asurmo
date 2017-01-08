43e76c5a3ee5792dc53397109ac219d08271aed4
MiselAdemi <ademimisel@gmail.com>
2017-01-08 17:23:22 +0100

    Album finished and organization fixing

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,15 +2,21 @@ class PicturesController < ApplicationController
   before_filter :authenticate_user!
   before_filter :find_user
   before_filter :find_album
-  before_filter :find_picture, :only => [ :show, :edit, :update ]
+  before_filter :find_picture, :only => [ :show, :edit, :update, :destroy ]
+  before_filter :add_breadcrumbs
   respond_to :html, :json
 
   def index
-    @pictures = Picture.all
+    @pictures = @album.pictures.all
+
+    respond_with(@pictures) do |format|
+      format.html { @pictures }
+      format.json { render :json => @pictures.as_json }
+    end
   end
 
   def show
-
+    add_breadcrumb @picture.caption
   end
 
   def new
@@ -22,6 +28,7 @@ class PicturesController < ApplicationController
     @picture.user = current_user
 
     if @picture.save
+      track_activity(@picture)
       respond_with(@picture) do |format|
         format.html { redirect_to album_pictures_path(@album) }
         format.json { render :json => @picture.as_json }
@@ -35,7 +42,16 @@ class PicturesController < ApplicationController
   end
 
   def update
+  end
 
+  def destroy
+    untrack_activity(@picture)
+    @picture.destroy
+
+    respond_to do |format|
+      format.html { redirect_to album_pictures_path(@album) }
+      format.json { head :no_content }
+    end
   end
 
   def url_options
@@ -43,6 +59,12 @@ class PicturesController < ApplicationController
   end
 
   private
+
+  def add_breadcrumbs
+    add_breadcrumb @user.first_name, user_path(@user)
+    add_breadcrumb "Albums", albums_path(@user)
+    add_breadcrumb "Pictures", album_pictures_path(@album)
+  end
 
   def find_user
     @user = User.friendly.find(params[:profile_name])
--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -1,2 +1,9 @@
 module AlbumsHelper
+  def album_thumbnail(album)
+    if album.pictures.count > 0
+      image_tag(album.pictures.first.picture.url)
+    else
+      image_tag("http://placekitten.com/260/180")
+    end
+  end
 end
