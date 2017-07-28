6ff79eb92397eaa262e7b70e87807fd867640d3a
Ademi Misel <ademimisel@gmail.com>
2017-07-28 13:41:39 +0200

    Fixing timelime

--- app/helpers/campains_helper.rb
+++ app/helpers/campains_helper.rb
@@ -1,2 +1,8 @@
 module CampainsHelper
+
+	def user_campains(user)
+		organizations = Organization.joins(:campains).where(:owner_id => user.id)
+		campains = organizations.map { |o| o.campains }.flatten
+	end
+
 end
