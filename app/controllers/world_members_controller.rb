b8fd06b1cd83008e5d14fa19a33257906991401d
Ademi Misel <ademimisel@gmail.com>
2017-08-07 13:13:18 +0200

    Delete member if not an asurmo member

--- app/controllers/world_members_controller.rb
+++ app/controllers/world_members_controller.rb
@@ -1,6 +1,7 @@
 class WorldMembersController < ApplicationController
 	  include WorldMembersHelper
 	before_action :find_organization
+	before_action :find_member, :only => [:destroy]
 
 	def new
 		
@@ -18,6 +19,11 @@ class WorldMembersController < ApplicationController
 		end
 	end
 
+	def destroy
+		@member.destroy
+		redirect_to organization_show_members_path(@organization)
+	end
+
 	def open_info_modal
   	@member = WorldMember.find(params[:world_member_id])
 	  respond_to do |format|
@@ -33,5 +39,9 @@ class WorldMembersController < ApplicationController
 
 	def find_organization
 		@organization = Organization.friendly.find(params[:organization_id])
+	end
+
+	def find_member
+		@member = WorldMember.find(params[:id])
 	end
 end
