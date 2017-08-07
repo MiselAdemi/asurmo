2af2c7a0d9b0b66eb22f98adb0289c97aa8decbc
Ademi Misel <ademimisel@gmail.com>
2017-08-07 13:30:09 +0200

    Update check if world member organization owner

--- app/models/world_member.rb
+++ app/models/world_member.rb
@@ -4,4 +4,9 @@ class WorldMember < ApplicationRecord
 	def is_platform_member?
 		User.exists?(email: self.email)
 	end
+
+	def is_organization_owner?(owner)
+		user = User.find(owner)
+		self.email == user.email
+	end
 end
