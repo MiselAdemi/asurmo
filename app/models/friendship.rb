b2e8f955fe629f58afc001ee5ccf10e38c7e915b
Ademi Misel <ademimisel@gmail.com>
2017-08-15 19:48:50 +0200

    Friend request notification

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,9 @@
 class Friendship < ApplicationRecord
 	belongs_to :user
 	belongs_to :friend, :class_name => "User"
+	after_create :notify_user
+
+	def notify_user
+		Notification.create(recipient: self.friend, user: User.find(self.user.id), action: "friend_request", notifiable: self.friend)
+	end
 end
