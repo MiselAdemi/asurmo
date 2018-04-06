560541ceab65765d2c2613f141a05c9f52871744
Misel Ademi <ademimisel@gmail.com>
2018-04-06 09:00:38 +0200

    Redesign root page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,8 @@ class HomeController < ApplicationController
       @activities = Activity.where("user_id = ? OR to_id = ?", current_user.id, current_user.id).order(:created_at => :desc).page(params[:page]).per(4)
       @notifications = Notification.where(:recipient => current_user).limit(8)
       @status = current_user.statuses.new
+      @album = current_user.albums.find_or_create_by(title: "Uncategorized")
+      @picture = @album.pictures.new
     end
   end
 
